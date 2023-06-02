c6325fa9bf95e4d8304d41d0dcc1ad92a5f06521
ccix24 <132009837+ccix24@users.noreply.github.com>
2023-06-02 05:33:48 -0400

    Update ChatPage.vue

--- src/pages/ChatPage.vue
+++ src/pages/ChatPage.vue
@@ -91,7 +91,7 @@ const sendMessage = async (inputData: string) => {
     stamp: dayjs().toISOString(),
     });
     }
-    const response = await axios.post<Message>('api/query', {
+    const response = await axios.post<Message>('api.py', {
       message:inputData,
     });
     const chatResponse = response.data;
